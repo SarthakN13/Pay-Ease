8fc5309105b5bb97ddf25593738ccc253cd5bfb0
Sarthak Nahar <163443733+SarthakN13@users.noreply.github.com>
2025-09-25 20:27:35 +0530

    Add files via upload

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { Appbar } from "../Components/Appbar"
+import { Balance } from "../Components/Balance"
+import { Users } from "../Components/Users"
+
+
+const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
+}
+
+export default Dashboard;
